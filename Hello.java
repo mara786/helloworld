79418ed6088c3f0503476471ef51e917c5a63fc0
mara786 <59841469+mara786@users.noreply.github.com>
2020-01-13 13:12:26 -0600

    hello is uploaded

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class Hello {
+ public static void main(String[]args) {
+	 System.out.println("Hello World");
+	 System.out.println("Why are you not talking back");
+	 System.out.println("boo");
+	 System.out.println("boo you lol");
+ }
+}
